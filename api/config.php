e3f18c82371d975f41c6609cbcd3e1c154ae960a
techSamuel <techsamuelsani@gmail.com>
2026-01-06 20:06:06 +0600

    Config: Set MySQL Timezone to +06:00

--- api/config.php
+++ api/config.php
@@ -35,6 +35,7 @@ try {
     $pdo = new PDO("mysql:host=" . DB_HOST . ";dbname=" . DB_NAME, DB_USER, DB_PASS);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+    $pdo->exec("SET time_zone = '+06:00';");
 } catch (PDOException $e) {
     die("ERROR: Could not connect. " . $e->getMessage());
 }
